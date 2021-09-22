b65ae4e9c79bc6f6bf39f606323a4b6480f312e6
Yashwanth reddy <yash868686reddy@gmail.com>
2021-09-21 23:20:41 -0400

    Initial commit

--- Memory-Game/app.js
+++ Memory-Game/app.js
@@ -1,16 +1,40 @@
 const grid = document.querySelector(".grid");
+const cards = Array.from(grid.querySelectorAll(".card"));
+const scoreSpan = document.querySelector('.score');
 
-const images = ["cheeseburger", "fries", "hotdog", "ice-cream", "milkshake", "pizza"];
+const images = [
+  "cheeseburger",
+  "fries",
+  "hotdog",
+  "ice-cream",
+  "milkshake",
+  "pizza",
+];
 
-function generateRandom(range){
-    return Math.floor(Math.random()*range + 1);
+let clicks = 0;
+let score = 0;
+const imageArr = [];
+
+function generateRandom(range) {
+  return Math.floor(Math.random() * range + 1);
 }
-function generateRandomImages(){
-    for (let i = 0; i < 16; i++) {
-        let img = document.createElement('img');
-        img.src = `./images/${images[generateRandom(images.length) - 1]}.png`;
-        grid.appendChild(img);
+function handleClick(e){
+    clicks++;
+    if(clicks <= 2){
+        imageArr.push(e.target.alt);
+    }else{
+        imageArr[0] === imageArr[1] ? scoreSpan.textContent = "1" : scoreSpan.textContent = "0"
     }
+    console.log(imageArr);
 }
 
-
+function generateRandomImages() {
+  cards.map((card) => {
+    let img = document.createElement("img");
+    const randomImage = images[generateRandom(images.length) - 1]
+    img.src = `./images/${randomImage}.png`;
+    img.alt = randomImage;
+    card.appendChild(img);
+    card.addEventListener('click', handleClick);
+  });
+}
