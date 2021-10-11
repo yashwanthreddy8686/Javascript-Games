cb83b0ee5030b388b8a5dd0f1cef46db469bfe22
YashwanthReddy <yash868686reddy@gmail.com>
2021-10-10 20:50:58 -0500

    Final Commit

--- Nokia3391-Snake/app.js
+++ Nokia3391-Snake/app.js
@@ -12,6 +12,60 @@ let speed = 0.9;
 let intervalTime = 0;
 let interval = 0;
 
+function startGame() {
+  currentSnake.forEach((index) => squares[index].classList.remove("snake"));
+  squares[appleIndex].classList.remove("apple");
+  clearInterval(interval);
+  score = 0;
+  randomApple();
+  direction = 1;
+  scoreDisplay.innerText = score;
+  intervalTime = 1000;
+  currentSnake = [2, 1, 0];
+  currentIndex = 0;
+  currentSnake.forEach((index) => squares[index].classList.add("snake"));
+  interval = setInterval(moveOutcomes, intervalTime);
+}
+
+function randomApple() {
+  do {
+    appleIndex = Math.floor(Math.random() * squares.length);
+  } while (squares[appleIndex].classList.contains("snake"));
+
+  squares[appleIndex].classList.add("apple");
+}
+
+function moveOutcomes() {
+  //If snake hit bottom
+  if (
+    (currentSnake[0] + width >= width * width && direction === width) ||
+    (currentSnake[0] % width === width - 1 && direction === 1) ||
+    (currentSnake[0] % width === 0 && direction === -1) ||
+    (currentSnake[0] - width < 0 && direction === -width) ||
+    squares[currentSnake[0] + direction].classList.contains("snake")
+  ) {
+    return clearInterval(interval);
+  }
+
+  const tail = currentSnake.pop();
+  squares[tail].classList.remove("snake");
+  currentSnake.unshift(currentSnake[0] + direction);
+
+  if (squares[currentSnake[0]].classList.contains("apple")) {
+    squares[currentSnake[0]].classList.remove("apple");
+    squares[tail].classList.add("snake");
+    currentSnake.push(tail);
+    randomApple();
+    score++;
+    scoreDisplay.textContent = score;
+    clearInterval(interval);
+    intervalTime = intervalTime * speed;
+    interval = setInterval(moveOutcomes, intervalTime);
+  }
+
+  squares[currentSnake[0]].classList.add("snake");
+}
+
 //assign functions to keycodes
 function control(e) {
   squares[currentIndex].classList.remove("snake");
@@ -26,5 +80,7 @@ function control(e) {
     direction = +width; //if we press down, the snake head will instantly appear in the div ten divs from where you are now
   }
 }
+
+startBtn.addEventListener("click", startGame);
 
 document.addEventListener("keyup", control);
